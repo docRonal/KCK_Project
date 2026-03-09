306ce74b6a7945d1248773192378178aa3d5188a
docRonal <258480@edu.p.lodz.pl>
2026-03-09 14:54:56 +0100

    first project steps

--- tlum/main.py
+++ tlum/main.py
@@ -9,3 +9,18 @@ engine.setProperty('volume', 1.0)
 
 def recognize(msg = "something"):
     r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        try:
+            recognised_text = r.recognize_google (audio, language='en-US')
+            print(recognised_text)
+            return recognised_text.lower()
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}",e)
+
+text = recognize()
+word_list = text.split()
+translator = Translator()
+
